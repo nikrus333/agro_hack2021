18d17ea28daa4de3af72da8f9ba7a077b4bfb172
nikrus333 <nikrus333@gmail.com>
2021-11-20 17:13:42 +0300

    add mask bin and pick box

--- found_box.py
+++ found_box.py
@@ -1,5 +1,7 @@
 import cv2
 import json
+import numpy as np
+import time
 
 global count
 def reverse_resize(data, scale_percent):
@@ -36,7 +38,8 @@ while(cap.isOpened()):
             dim = (width, height)
 
             resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-        
+            resized_clear = resized
+            frame_resized = resized
             cv2.namedWindow('resized')
             cv2.setMouseCallback('resized',draw_circle)
             if count == 1:
@@ -58,17 +61,47 @@ while(cap.isOpened()):
             color3 = (0, 0, 255)
             thickness = 2
         
-            frame = cv2.rectangle(resized, rectangle_water_1[0], rectangle_water_1[1], color1, thickness)
-            frame = cv2.rectangle(resized, rectangle_water_2[0], rectangle_water_2[1], color1, thickness)
-            frame = cv2.rectangle(resized, rectangle_eat[0], rectangle_eat[1], color2, thickness)
-            frame = cv2.rectangle(resized, rectangle_wall[0], rectangle_wall[1], color3, thickness)
+            end_frame = cv2.rectangle(frame_resized, rectangle_water_1[0], rectangle_water_1[1], color1, thickness)
+            end_frame = cv2.rectangle(frame_resized, rectangle_water_2[0], rectangle_water_2[1], color1, thickness)
+            end_frame = cv2.rectangle(frame_resized, rectangle_eat[0], rectangle_eat[1], color2, thickness)
+            end_frame = cv2.rectangle(frame_resized, rectangle_wall[0], rectangle_wall[1], color3, thickness)
 
+            cv2.imshow('resized', end_frame)
+            points_water_1 = np.array([[0,0],[0,850],[850,850],[850,0] ],
+                                        dtype=np.int32)
+            frame_bin = cv2.fillPoly(resized_clear, [points_water_1], (0, 0, 0))
+            points_water_1 = np.array([[rectangle_water_1[0][0], rectangle_water_1[0][1]], 
+                                        [rectangle_water_1[1][0],rectangle_water_1[0][1]], 
+                                        [rectangle_water_1[1][0],rectangle_water_1[1][1]], 
+                                        [rectangle_water_1[0][0],rectangle_water_1[1][1]]],
+                                        dtype=np.int32)
+            frame_bin = cv2.fillPoly(resized_clear, [points_water_1], (255, 255, 255))
+            
+            points_water_2 = np.array([[rectangle_water_2[0][0], rectangle_water_2[0][1]], 
+                                        [rectangle_water_2[1][0],rectangle_water_2[0][1]], 
+                                        [rectangle_water_2[1][0],rectangle_water_2[1][1]], 
+                                        [rectangle_water_2[0][0],rectangle_water_2[1][1]]],
+                                        dtype=np.int32)
+            frame_bin = cv2.fillPoly(resized_clear, [points_water_2], (255, 255, 255))
+
+            points_eat = np.array([[rectangle_eat[0][0], rectangle_eat[0][1]], 
+                                        [rectangle_eat[1][0],rectangle_eat[0][1]], 
+                                        [rectangle_eat[1][0],rectangle_eat[1][1]], 
+                                        [rectangle_eat[0][0],rectangle_eat[1][1]]],
+                                        dtype=np.int32)
+            frame_bin = cv2.fillPoly(resized_clear, [points_eat], (255, 255, 255))
+
+            frame_bin = cv2.cvtColor(frame_bin,cv2.COLOR_BGR2GRAY)
+            ret, frame_bin = cv2.threshold(frame_bin, 254, 255, 0)
             # resized_test = cv2.resize(frame_test, dim, interpolation = cv2.INTER_AREA)
             # frame_test = cv2.rectangle(resized_test, rectangle_water_1[0], rectangle_water_1[1], color1, thickness)
             # frame_test = cv2.rectangle(resized_test, rectangle_water_2[0], rectangle_water_2[1], color1, thickness)
             # frame_test = cv2.rectangle(resized_test, rectangle_eat[0], rectangle_eat[1], color2, thickness)
-
-            cv2.imshow('resized', frame)
+            imgWeight = 1.5
+            gaussianWeight = -0.5
+            gaussian = cv2.GaussianBlur(frame_bin, (5,5), 0)
+            frame_bin = cv2.addWeighted(frame_bin, imgWeight, gaussian, gaussianWeight, 0)
+            cv2.imshow('resized_bin', frame_bin)
             #cv2.imshow('test', frame_test)
             rectangle_water_1_old = reverse_resize(rectangle_water_1, 50)
             rectangle_water_2_old = reverse_resize(rectangle_water_2, 50)
@@ -82,6 +115,7 @@ while(cap.isOpened()):
                 "eat":rectangle_eat_old,
                 "wall":rectangle_wall_old
                 }, outfile)
+            time.sleep(0.1) 
     except Exception as e:
         print(e)
    
